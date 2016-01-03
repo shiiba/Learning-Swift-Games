4b7ed4d31a3bbf22e3f6bdb836171d161ede78b5
Shiiba <kshiiba@gmail.com>
2016-01-02 22:37:33 -0500

    Initial Commit

---
+++ ConnectFour/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConnectFour
+//
+//  Created by Kevin Shiiba on 1/2/16.
+//  Copyright © 2016 Kevin Shiiba. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
