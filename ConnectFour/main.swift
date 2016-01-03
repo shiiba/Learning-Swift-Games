b97ed4305379921f88434ea781613974416739ec
Shiiba <kshiiba@gmail.com>
2016-01-03 00:26:45 -0500

    Initial Skeleton of the app

--- ConnectFour/main.swift
+++ ConnectFour/main.swift
@@ -1,3 +1,4 @@
+#!/usr/bin/swift
 //
 //  main.swift
 //  ConnectFour
@@ -8,5 +9,83 @@
 
 import Foundation
 
-print("Hello, World!")
+typealias Board = Array<Array<Cell>>
 
+enum CellContents: String {
+    case X
+    case O
+    case Empty
+    
+    var description: String {
+        switch self {
+        case X:
+            return "X"
+        case O:
+            return "O"
+        case Empty:
+            return " "
+        }
+    }
+}
+
+struct Cell {
+    var value: CellContents
+}
+
+let maxColumns = 6
+let maxRows = 6
+
+// declare players (boolean?), after set boolean to !boolean; (e.g. if "true" = "O", if "false" = "X")
+
+func printBoard(board: Board) {
+    for row in 0...maxRows-1 {
+        print("|", terminator:"")
+        for column in 0...maxColumns-1 {
+            print(board[row][column].value.description, terminator:"|")
+        }
+        print("")
+    }
+}
+
+
+let args = Process.arguments
+
+func main(args: [String]) {
+    var board = Board()
+    var input = ""
+    
+    // Initialize board
+    for _ in 0...maxColumns-1 {
+        board.append(Array(count: maxRows, repeatedValue: Cell(value: .Empty)))
+    }
+    printBoard(board)
+    
+    // game loop
+    repeat {
+        // Player "X", which column do you play?; take into account human entry preferences
+        input = readLine(stripNewline: true)!
+        // take input (columnIndex), and iterate (bottom up) through that column, checking for empty; if empty, change to current player enum value
+        printBoard(board)
+
+    } while (input != "END") // solver in while loop;
+    
+    // declare winner
+    // human error handling: what if they say "Z" in column number?
+}
+
+main(args)
+
+
+
+
+
+    
+    
+
+//|  |  |  |  |  |  |
+//|  |  |  |  |  |  |
+//|  |  |  |  |  |  |
+//|  |  |  |  |  |  |
+//|  |  |  |  |  |  |
+//|X |  |  |  |  |  |
+//-------------------
